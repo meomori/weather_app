5efef9b9fdeddddb91fa9111e7b896d8a77e7494
Manatsu E. Omori <manatsu.e.omori@gmail.com>
2017-07-17 10:18:10 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'test' => 'welcome#test'
+
+  root 'welcome#index'
+
+  get 'index' => 'welcome#index'
+
+  post 'index' => 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
